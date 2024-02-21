c53c82b8be8ba30502bdb7448bca0c9d706944dd
Imtiyaaz-D <129970424+Imtiyaaz-D@users.noreply.github.com>
2024-02-21 10:34:18 +0200

    file structure

---
+++ imtiportfoliov2/src/components/FooterComp.vue
@@ -0,0 +1,37 @@
+<template>
+   <footer id="footer">
+            <div id="copyright">
+               
+                <div class="icons">
+                    <a href="https://github.com/Imtiyaaz-D" target="_blank"><i class="bi bi-github"></i></a>
+                <a href="https://www.linkedin.com/feed/" target="_blank"><i class="bi bi-linkedin"></i></a>
+                <a href="mailto:imtidamon777@gmail.com" target="_blank"><i class="bi bi-envelope-at"></i></a>
+                </div>
+                <p>@POWERED BY Imti-Damon COPYRIGHT &COPY; {{cYear}}</p>
+            </div>
+        </footer>
+    
+</template>
+
+<script>
+export default {
+    data() {
+            return{
+                cYear: new Date().getUTCFullYear()
+            }
+        }
+}
+</script>
+
+<style>
+.bi:hover {
+    color: red;
+}
+
+.bi {
+    font-size: 1.8rem;
+    margin: 0.5rem;
+    color: black;
+}
+
+</style>
---
+++ imtiportfoliov2/src/components/NavbarComp.vue
@@ -0,0 +1,45 @@
+<template>
+   <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <img src="https://iili.io/J1JKzxe.png" :style="{marginLeft:'144px'}" alt="logo" loading="lazy">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <router-link to="/">Home</router-link>
+                  </li>
+                  <li class="nav-item">
+                  <router-link to="/about">About</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link to="/skills">Skills</router-link>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <router-link to="/project">Projects</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link to="/testimonial">Testimonial</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link to="/contact">Contact</router-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
