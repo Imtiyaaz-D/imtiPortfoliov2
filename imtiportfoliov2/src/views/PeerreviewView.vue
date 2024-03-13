7717e706de0e499e692dd2cac2469443f61b29f4
Imtiyaaz-D <129970424+Imtiyaaz-D@users.noreply.github.com>
2024-03-13 10:00:23 +0200

    edit img
    changed the img sizing of the peerreview

--- imtiportfoliov2/src/views/PeerreviewView.vue
+++ imtiportfoliov2/src/views/PeerreviewView.vue
@@ -34,25 +34,6 @@
     mounted() {
       this.$store.dispatch('fetchTestimonials');
     },
-    methods: {
-      // attempt to make an on scroll feature
-      // window.addEventListener('scroll',this.handleScroll);
-      // },
-      // beforeUnmount(){
-      //     window.removeEventListener('scroll',this.handleScroll);
-      // },
-      // methods: {
-      //     handleScroll(){
-      //         const containerPostion = this.$refs.testimonialContainer.getBoundingClientRect();
-      //         const scrollThreshold = 200;
-      //         if(containerPostion.top < scrollThreshold){
-      //             this.isScrolled = true;
-      //         }else{
-      //             this.isScrolled = false;
-      //         }
-      //     }
-      // }
-    },
   };
   </script>
   
@@ -75,11 +56,11 @@
     justify-content: center;
     margin-left: 15rem;
   }
-  .testimonial.scrolled {
+  /* .testimonial.scrolled {
     opacity: 0.5;
     transform: translateY(-20px);
     transition: opacity 2.6s ease, transform 2.6s ease;
-  }
+  } */
   .p {
     width: 17rem;
     text-align: center;
@@ -87,14 +68,13 @@
   
   .card-body {
     text-align: center;
-    width: 20rem;
+    /* width: 20rem; */
     margin-right: 3=4rem;
   }
   
   .image {
-    margin-left: 0.1rem;
     position: relative;
-    height: 20rem;
+    height: 23rem;
     width: 100%;
     overflow: hidden;
   }
@@ -102,7 +82,7 @@
   .card-img-top {
     width: 100%;
     height: 100%;
-    object-fit: contain;
+    /* object-fit: contain; */
   }
   
   .card.border.border-dark {
